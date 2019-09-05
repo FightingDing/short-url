46e9df8e985d270c697c9fa20ae4e16268817e76
xiaobai <xiaobai@xiaobai.com>
2019-09-05 17:07:31 +0800

    init

---
+++ src/main/java/com/suitwe/shorturl/controller/ShortUrlController.java
@@ -0,0 +1,123 @@
+package com.suitwe.shorturl.controller;
+
+import com.suitwe.shorturl.domain.Result;
+import com.suitwe.shorturl.domain.UrlRequest;
+import com.suitwe.shorturl.service.ShortUrlService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.net.InetAddress;
+import java.util.Random;
+
+/**
+ * 短连接操作控制器
+ *
+ * @author cheivin
+ * @version 1.0
+ * @date 2018/6/7
+ */
+@Controller
+public class ShortUrlController {
+    @Autowired
+    private ShortUrlService shortUrlService;
+
+    @GetMapping("/")
+    public ModelAndView index(){
+        return new ModelAndView("forward:idx.html");
+    }
+
+    @ResponseBody
+    @PostMapping("/shortUrl")
+    public Result generateShortUrl(@Valid UrlRequest urlRequest, HttpServletRequest request) {
+        String ip = getIpAddr(request);
+        if (!StringUtils.isEmpty(urlRequest.getTag())) {
+            if (shortUrlService.generateShortUrl(urlRequest.getTag(), urlRequest.getUrl(),ip)) {
+                return new Result(200, "生成成功！", urlRequest.getTag());
+            } else {
+                return new Result(0, "生成失败，该短链接已被使用", null);
+            }
+        } else {
+            // 填充默认值
+            if (urlRequest.getType() == null) {
+                urlRequest.setType(-1);
+            }
+            if (urlRequest.getLength() == null) {
+                urlRequest.setLength(4 + (new Random()).nextInt(12));
+            }
+            if (urlRequest.getGenerator() == null) {
+                urlRequest.setGenerator(1);
+            }
+
+            // 生成短链接
+            String tag = shortUrlService.generateShortUrl(urlRequest.getUrl(), urlRequest.getType(), urlRequest.getLength(), urlRequest.getGenerator(),ip);
+            if (StringUtils.isEmpty(tag)) {
+                return new Result(0, "生成失败，请重试", null);
+            } else {
+                return new Result(200, "生成成功", tag);
+            }
+        }
+    }
+
+    /**
+     * 短链接访问操作，如果不存在记录则跳转到主页
+     *
+     * @param tag 短连接标识
+     */
+    @GetMapping("/{tag:[0-9a-zA-Z]{4,16}}")
+    public ModelAndView setShortUrl(@PathVariable("tag") String tag, HttpServletRequest request) {
+        String url = shortUrlService.getUrl(tag);
+        if (url == null) {
+            return new ModelAndView("forward:idx.html");
+        } else {
+            Logger.getLogger(this.getClass()).info("IP:[" + getIpAddr(request) + "] 短连接访问：短连接:" + tag + ",原始链接:" + url);
+            return new ModelAndView("redirect:" + url);
+        }
+    }
+
+
+    /**
+     * 获取ip地址
+     *
+     * @param request 请求参数
+     * @return ip地址
+     */
+    private String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+            if (ip.equals("127.0.0.1")) {
+                //根据网卡取本机配置的IP
+                InetAddress inet = null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                ip = inet.getHostAddress();
+            }
+        }
+        // 多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        if (ip != null && ip.length() > 15) {
+            if (ip.indexOf(",") > 0) {
+                ip = ip.substring(0, ip.indexOf(","));
+            }
+        }
+        return ip;
+    }
+
+}
---
+++ src/main/java/com/suitwe/shorturl/service/impl/ShortUrlServiceImpl.java
@@ -0,0 +1,123 @@
+package com.suitwe.shorturl.service.impl;
+
+import com.suitwe.shorturl.dao.ShortUrlDao;
+import com.suitwe.shorturl.entity.ShortUrl;
+import com.suitwe.shorturl.service.ShortUrlOpsService;
+import com.suitwe.shorturl.service.ShortUrlService;
+import com.suitwe.shorturl.utils.convert.ConvertUtil;
+import com.suitwe.shorturl.utils.convert.Md5ConvertUtil;
+import com.suitwe.shorturl.utils.convert.UuidConvertUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 短网址操作实现
+ *
+ * @author cheivin
+ * @version 1.0
+ * @date 2018/6/7
+ */
+@Service
+public class ShortUrlServiceImpl implements ShortUrlService {
+    private final ShortUrlDao shortUrlDao;
+    private final ShortUrlOpsService shortUrlOpsService;
+
+    @Autowired
+    public ShortUrlServiceImpl(ShortUrlDao shortUrlDao, ShortUrlOpsService shortUrlOpsService) {
+        this.shortUrlDao = shortUrlDao;
+        this.shortUrlOpsService = shortUrlOpsService;
+    }
+
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public boolean generateShortUrl(String tag, String url, String ip) {
+        // 判断是否存在该key
+        if (shortUrlOpsService.exist(tag)) {
+            return false;
+        }
+        ShortUrl shortUrl = new ShortUrl(tag, url, 0, ip, new Date());
+        // 存数据库
+        try {
+            shortUrlDao.save(shortUrl);
+        } catch (Exception e) {
+            return false;
+        }
+        // 放入缓存
+        shortUrlOpsService.setUrl(tag, url);
+        return true;
+    }
+
+    @Override
+    public String generateShortUrl(String url, int type, int length, int generator, String ip) {
+        ConvertUtil convertUtil;
+        switch (generator) {
+            case 0:
+                convertUtil = Md5ConvertUtil.getInstance();
+                break;
+            case 1:
+                convertUtil = UuidConvertUtil.getInstance();
+                break;
+            default:
+                convertUtil = Md5ConvertUtil.getInstance();
+        }
+
+        String[] tags = convertUtil.shortString(url, type, length);
+        for (String tag : tags) {
+            if (generateShortUrl(tag, url, ip)) {
+                return tag;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    /**
+     * 每小时存储一次
+     */
+    @Scheduled(cron = "0 0/1 * * * ? ")
+    public void saveCount2Db() {
+        // 获取缓存访问计数
+        Map<Object, Object> map = shortUrlOpsService.getAllCount();
+        LinkedList<String> tagList = new LinkedList<>();
+        map.forEach((k, v) -> tagList.add(k.toString()));
+
+        // 查询对象列表
+        List<ShortUrl> shortUrlList = shortUrlDao.findByTagIn(tagList);
+
+        // 更新访问计数
+        shortUrlList.forEach(url -> url.setCount(Integer.valueOf((String) map.get(url.getTag()))));
+
+        // 分批存入数据库，5000条一次
+        int times = (int) Math.ceil(shortUrlList.size() / 5000.0);
+        for (int i = 0; i < times; i++) {
+            List<ShortUrl> urlList = shortUrlList
+                    .subList(i * 5000, (i + 1) * 5000 < shortUrlList.size() ? (i + 1) * 5000 : shortUrlList.size());
+            shortUrlDao.save(urlList);
+        }
+    }
+
+    @Override
+    public String getUrl(String tag) {
+        if (shortUrlOpsService.exist(tag)) {
+            shortUrlOpsService.increment(tag);
+            return shortUrlOpsService.getUrl(tag);
+        } else {
+            ShortUrl shortUrl = shortUrlDao.findByTag(tag);
+            if (shortUrl == null) {
+                return null;
+            } else {
+                shortUrlOpsService.setUrl(tag, shortUrl.getUrl());
+                shortUrlOpsService.setCount(tag, shortUrl.getCount() + 1);
+                return shortUrl.getUrl();
+            }
+        }
+    }
+}
